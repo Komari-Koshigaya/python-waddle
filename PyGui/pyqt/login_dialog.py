66d078b886c49f15282aadf5a5f9bd91aa4e1b47
niejun <2323384399@qq.com>
2020-08-28 11:51:52 +0800

    feat: now it can be run when not install PyQt5 firstly, and using lambda to bind button event.

--- PyGui/pyqt/login_dialog.py
+++ PyGui/pyqt/login_dialog.py
@@ -24,21 +24,29 @@ class LoginDialog(QMainWindow, Ui_Dialog):
     """
     登录界面的控制逻辑
     """
+
     def __init__(self, parent=None):
         super(LoginDialog, self).__init__(parent)
+        self.dict_win = DictWin()  # 实例化一个主窗口界面
         self.setupUi(self)
         self.setWindowIcon(QIcon('b.ico'))
         self.login_btn.setStyleSheet('''color: white; background-color: rgb(218,181,150);''')
         self.regi_btn.setStyleSheet('''color: white; background-color: rgb(218,181,150);''')
-        # 实例化一个主窗口界面
-        self.dict_win = DictWin()
-        self.login_btn.clicked.connect(self.login_event)  # 绑定登陆函数
 
-    def login_event(self):
+        """
+        绑定函数有两种写法 
+        1. self.login_btn.clicked.connect( lambda: self.login_event('argument') )  可以传参，推荐使用
+        2. self.login_btn.clicked.connect(self.login_event)  无法传参，login_event()也不能有self之外的参数，否则会报错
+        """
+        # self.login_btn.clicked.connect(self.login_event)  # 使用方法二 绑定登陆函数
+        self.login_btn.clicked.connect(lambda: self.login_event(name='小小怪'))  # 使用方法一 绑定登陆函数
+
+    def login_event(self, name):
         """
         点击登录时，进行登录验证
         :return: None
         """
+        logger.info("触发登录事件，并传递了参数：" + name)
         if self.name_edit.text() == "":
             QMessageBox.about(self, '登陆', '请输入姓名')
         elif self.pwd_edit.text() == "":
@@ -46,6 +54,8 @@ class LoginDialog(QMainWindow, Ui_Dialog):
         else:
             QMessageBox.information(self, '登陆', self.name_edit.text() + ' 欢迎登陆')
             logger.debug(self.name_edit.text() + "登录成功，将退出登录界面，跳转到主界面！")
+            # dict_win = DictWin()
+
             self.dict_win.show(self.name_edit.text())  # 显示主窗口, 并传递登录的用户名
             self.close()  # 关闭登录窗口
 
--- PyGui/pyqt/main.py
+++ PyGui/pyqt/main.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 # python的gui编程
 
-import sys
-from PyQt5.QtWidgets import QApplication
-from login_dialog import LoginDialog
-
 # 主程序，先出现登录界面，登录成功则跳转到主界面
 if __name__ == '__main__':
+    import sys
+    from login_dialog import LoginDialog
+
+    try:
+        from PyQt5.QtWidgets import QApplication
+    except ModuleNotFoundError as e:
+        # 执行时引入 pyqt5库失败时，则使用 pip指令安装
+        import os
+        print('本机未安装pyqt，正在安装 PyQt5 ...')
+        os.system(
+            'pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn pyqt5')
+
     app = QApplication(sys.argv)
-    login_dia = LoginDialog()   # 实例化登录窗口
+    login_dia = LoginDialog()  # 实例化登录窗口
     login_dia.show()
     app.exec_()
